f022368d86b7e9719ec2e18e6a065e7a7db66927
Yousef Mahmoud <usef.mahmoud@outlook.com>
2023-04-25 06:47:22 +0200

    navbar links and styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
 import './Navbar.scss'
+import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="navbar__links">
+        <div className="link">
+          <NavLink to='/' activeClassName='active'>HOME</NavLink>
+        </div>
+        <span className="dot">•</span>
+        <div className="link">
+          <NavLink to='/projects' activeClassName='active'>PROJECTS</NavLink>
+        </div>
+        <span className="dot">•</span>
+        <div className="link">
+          <NavLink to='/about' activeClassName='active'>ABOUT</NavLink>
+        </div>
+        <span className="dot">•</span>
+        <div className="link">
+          <NavLink to='/contact' activeClassName='active'>CONTACT</NavLink>
+        </div>
+      </div>
+    </div>
   )
 }
 
