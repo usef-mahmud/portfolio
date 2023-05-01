838673b146aa7dde0eaf984fbf36e405b5bede4d
Yousef Mahmoud <usef.mahmoud@outlook.com>
2023-05-01 18:01:05 +0200

    add some fonts, contact form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,7 +3,32 @@ import React from 'react'
 import './Contact.scss'
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact-page'>
+
+      <div className="contact-form">
+        <div className="title">
+          let's have a talk
+        </div>
+        <div className="form-box">
+          <div className="text-box">
+            <input type="text" />
+            <span className='active'>name</span>
+          </div>
+        </div>
+        <div className="form-box">
+          <div className="text-box">
+            <textarea rows="5"></textarea>
+            <span>message</span>
+          </div>
+        </div>
+        <div className="form-box">
+          <div className="send-btn">
+            send
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
